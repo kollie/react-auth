630d57eee32a5f5f606d9f232e93be9bb0aee147
kollie <zikollie@gmail.com>
2017-10-04 19:29:18 +0000

    Add functionality for login
    Handle user inputs
    Login users
    Give users feedback activity

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+        apiKey: 'AIzaSyDUM-zhi8Epf2KI7FSMuEnym_871n0ng-U',
+        authDomain: 'auth-f26d7.firebaseapp.com',
+        databaseURL: 'https://auth-f26d7.firebaseio.com',
+        projectId: 'auth-f26d7',
+        storageBucket: 'auth-f26d7.appspot.com',
+        messagingSenderId: '608313609688'
+      });    
+    }
+
     render() {
         return (
             <View>
-                <Text>An app</Text>
+                <Header headerText='Authentication' />
+                <LoginForm />
             </View>
         );
     }
 }
 
 export default App;
+
